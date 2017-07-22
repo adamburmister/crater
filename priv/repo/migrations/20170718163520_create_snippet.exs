a6ce6f03eb52657e2dfbbf3c5bb7cf1535bb1be4
Resi Respati <resir014@gmail.com>
2017-07-22 11:21:09 +0700

    :beetle: (migration) reordered where the timestamp column is located

--- priv/repo/migrations/20170718163520_create_snippet.exs
+++ priv/repo/migrations/20170718163520_create_snippet.exs
@@ -3,12 +3,15 @@ defmodule Crater.Repo.Migrations.CreateSnippet do
 
   def change do
     create table(:snippets, primary_key: false) do
+      # Timestamps should be inserted immediately after the primary key in the
+      # table. This ensures that when we add a new migration to the same table,
+      # we have a clean data when we SELECT them.
       add :id, :uuid, primary_key: true
+      timestamps()
+
       add :title, :string
       add :body, :text
       add :description, :text
-
-      timestamps()
     end
 
   end
