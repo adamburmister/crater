d3f71d6f28d9f72c83199c4d4b708e91c131cb46
Resi Respati <resir014@gmail.com>
2017-08-05 20:43:27 +0700

    :beetle: primary key isn't correctly set in Snippet context

--- lib/crater/snippets/snippet.ex
+++ lib/crater/snippets/snippet.ex
@@ -3,6 +3,9 @@ defmodule Crater.Snippets.Snippet do
   import Ecto.Changeset
   alias Crater.Snippets.Snippet
 
+  # :binary_id is managed by drivers/adapters, it will be UUID for mysql
+  # but can be ObjectID if later you decide to use mongo
+  @primary_key {:id, :binary_id, autogenerate: true}
 
   schema "snippets" do
     field :body, :string
@@ -17,6 +20,6 @@ defmodule Crater.Snippets.Snippet do
   def changeset(%Snippet{} = snippet, attrs) do
     snippet
     |> cast(attrs, [:title, :language, :body, :description])
-    |> validate_required([:title, :language, :body, :description])
+    |> validate_required([:title, :language, :body])
   end
 end
